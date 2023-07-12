fd238578c235b8dea11c443bd2d4d168c08cb55a
lake <lakehu0x@gmail.com>
2023-07-12 18:01:04 +0800

    t

--- src/api/index.ts
+++ src/api/index.ts
@@ -23,18 +23,24 @@ export const createPropose = async ({
  })
 }
 
+const idUnitLen = 'u64'.length
 
 export const getList = async () => {
     const mappingName = 'proposal_ids'
     const key = '2074281269322187893875field'
     const api = `https://vm.aleo.org/api/testnet3/program/${programID}/mapping/${mappingName}/${key}`
     const res = await fetch(api).then(res => res.json())
-    return res
+    const id = +res.slice(0, -idUnitLen)
+    const ids = Array.from({ length: id + 1}, (_, i) => i)
+    const details = await Promise.all(ids.map(id => getDetail(id)))
+    const filterDetails = details.filter(Boolean)
+    // console.log({ids, filterDetails})
+    return filterDetails
 }
 
-export const getDetail = async (id: string) => {
+export const getDetail = async (id: string | number) => {
     const mappingName = 'proposals'
-    const key = `${id}field`
+    const key = `${id}u64`
     const api = `https://vm.aleo.org/api/testnet3/program/${programID}/mapping/${mappingName}/${key}`
     return await fetch(api).then(res => res.json())
 }
